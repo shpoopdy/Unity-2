f3ccb6562ba1394141e253f1ba69f0ba126398ab
Mikey <shpoopdy@gmail.com>
2023-01-05 11:24:42 -0800

    Add audio, update Movement.cs

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
   Rigidbody rb;
+  AudioSource audioSource;
   [SerializeField] float mainThrust = 100f;
   [SerializeField] float rotationThrust = 100f;
   // Start is called before the first frame update
   void Start()
   {
     rb = GetComponent<Rigidbody>();
+    audioSource = GetComponent<AudioSource>();
   }
 
   // Update is called once per frame
@@ -23,7 +25,12 @@ public class Movement : MonoBehaviour
   void ProcessThrust() {
     if (Input.GetKey(KeyCode.Space)) {
       rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-    }
+      if (!audioSource.isPlaying) {
+        audioSource.Play();
+      }
+    } else {
+        audioSource.Stop();
+      }
   }
 
   void ProcessRotation() {
