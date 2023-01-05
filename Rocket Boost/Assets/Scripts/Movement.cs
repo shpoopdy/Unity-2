58630a515681ba1377244146b499c134230a8dc7
Mikey <shpoopdy@gmail.com>
2023-01-04 18:35:25 -0800

    Add rotation methods

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
   Rigidbody rb;
+  [SerializeField] float mainThrust = 100f;
+  [SerializeField] float rotationThrust = 100f;
   // Start is called before the first frame update
   void Start()
   {
@@ -20,15 +22,20 @@ public class Movement : MonoBehaviour
 
   void ProcessThrust() {
     if (Input.GetKey(KeyCode.Space)) {
-      rb.AddRelativeForce(Vector3.up);
+      rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
     }
   }
 
   void ProcessRotation() {
     if (Input.GetKey(KeyCode.D)) {
-      Debug.Log("Going RIGHT!");
-    } else if (Input.GetKey(KeyCode.A)) {
-      Debug.Log("Going LEFT!");
+      ApplyRotation(-rotationThrust);
     }
+    else if (Input.GetKey(KeyCode.A)) {
+      ApplyRotation(rotationThrust);
+    }
+  }
+
+  void ApplyRotation(float rotationThisFrame) {
+    transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
   }
 }
