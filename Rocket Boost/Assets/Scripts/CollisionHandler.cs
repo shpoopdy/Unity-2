e0d672c275dfe646e820f367eda7706ca0867ed6
Mikey <shpoopdy@gmail.com>
2023-01-05 13:29:01 -0800

    Create second level

--- Rocket Boost/Assets/Scripts/CollisionHandler.cs
+++ Rocket Boost/Assets/Scripts/CollisionHandler.cs
@@ -3,26 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other) {
-      switch(other.gameObject.tag) {
-        case "Friendly":
-          Debug.Log("I'm friendly");
-          break;
-        case "Finished":
-          Debug.Log("Level Done!");
-          break;
-        case "Fuel":
-          Debug.Log("Fuel Added!");
-          break;
-        default:
-          ReloadLevel();
-          break;
-      }
+  void OnCollisionEnter(Collision other) {
+    switch(other.gameObject.tag) {
+      case "Friendly":
+        Debug.Log("I'm friendly");
+        break;
+      case "Finished":
+        LoadNextLevel();
+        break;
+      case "Fuel":
+        Debug.Log("Fuel Added!");
+        break;
+      default:
+        ReloadLevel();
+        break;
     }
+  }
 
     void ReloadLevel() {
-       //Gets the scene we are currently on.
+      //Gets the scene we are currently on.
       int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
       SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel() {
+      int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+      int nextSceneIndex = currentSceneIndex + 1;
+      if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+        nextSceneIndex = 0;
+      }
+      SceneManager.LoadScene(nextSceneIndex);
     }
 }
